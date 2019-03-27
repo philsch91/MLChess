0192e3cc4824778ab64a768fce80a993491e4f92
Philipp Schunker <philipp@schunker.at>
2019-03-27 20:44:55 +0100

    Initial Commit

---
+++ MLChess/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MLChess
+//
+//  Created by Philipp Schunker on 27.03.19.
+//  Copyright © 2019 Philipp Schunker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MLChess/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MLChess
+//
+//  Created by Philipp Schunker on 27.03.19.
+//  Copyright © 2019 Philipp Schunker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
